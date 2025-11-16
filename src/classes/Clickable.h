ebfed80395cab7cf86af982d2f073b915d4ef840
notmike15 <31933319+notmike15@users.noreply.github.com>
2025-11-16 10:57:10 -0500

    Refactored so clickables inherit from objects

--- src/classes/Clickable.h
+++ src/classes/Clickable.h
@@ -4,12 +4,17 @@
 
 #ifndef SFMLCODINGPROJECT_CLICKABLE_H
 #define SFMLCODINGPROJECT_CLICKABLE_H
+#include <utility>
+
+#include "Object.h"
 #include "SFML/Window/Window.hpp"
 
 namespace NMGP {
-    class Clickable {
+    class Clickable : public Object {
     public:
-        virtual ~Clickable() = default;
+        Clickable(sf::Texture texture, LAYER layer, sf::Vector2f pos, sf::Vector2f scale)  :
+            Object(std::move(texture), layer, pos, scale) {};
+        ~Clickable() override = default;
 
         virtual int onClick() = 0;
         virtual int onHover() = 0;
