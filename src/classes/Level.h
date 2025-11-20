18222b98496c5b545b84313b15a2714c7641fc0f
gavin <mynamesnotmike15@gmail.com>
2025-11-20 13:23:32 -0500

    Added Active Level setting to determine which level type it is to be checked against game manager Also some small changes for function handling

--- src/classes/Level.h
+++ src/classes/Level.h
@@ -11,22 +11,33 @@
 #include "SFML/Graphics/RenderWindow.hpp"
 #include <memory>
 
+#include "Options.h"
+
 namespace NMGP {
     class Level {
+    public:
+        enum ACTIVELEVEL {
+            MAIN_MENU = 1,
+            OPTIONS = 2,
+            PLAY = 3
+        };
     private:
         std::vector<std::shared_ptr<Object>> objects;
         int levelID;
+        ACTIVELEVEL levelActiveView;
     public:
         static bool comp(std::shared_ptr<Object> a, std::shared_ptr<Object> b);
-        Level(std::vector<std::shared_ptr<Object>> objs, int id);
-        Level() : Level(std::vector<std::shared_ptr<Object>>(), 0) {};
+        Level(std::vector<std::shared_ptr<Object>> objs, int id, ACTIVELEVEL lv);
+        Level() : Level(std::vector<std::shared_ptr<Object>>(), 0, ACTIVELEVEL::MAIN_MENU) {};
         ~Level();
-        int getLevelID() const { return levelID; };
+        [[nodiscard]] int getLevelID() const { return levelID; };
         std::shared_ptr<Object> getObjectById(int id);
         std::shared_ptr<Object> getObjectByName(std::string name);
         std::vector<std::shared_ptr<Object>> getObjects() { return objects; };
         void addObject(std::shared_ptr<Object> object);
         std::shared_ptr<Object> getClickedOn(sf::Vector2f mousePosition);
+        void setActiveLevel(ACTIVELEVEL lv);
+        [[nodiscard]] ACTIVELEVEL getActiveLevel() const { return levelActiveView; };
         void draw(sf::RenderWindow* window);
     };
 }
