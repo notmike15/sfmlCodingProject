84f9a3ca9e88395709e2cac85c0b8abf84d98473
notmike15 <31933319+notmike15@users.noreply.github.com>
2025-11-20 21:52:25 -0500

    Added font object to constants for consistent reference

--- src/headers/Constants.h
+++ src/headers/Constants.h
@@ -5,6 +5,8 @@
 #ifndef SFMLCODINGPROJECT_CONSTANTS_H
 #define SFMLCODINGPROJECT_CONSTANTS_H
 #include <string>
+
+#include "SFML/Graphics/Font.hpp"
 #include "SFML/Graphics/Texture.hpp"
 
 namespace Constants {
@@ -18,7 +20,7 @@ namespace Constants {
     static const std::string REBUILDING_OPTIONS_FILE = "Unable to pull options file, re-building...\n";
     static const std::string FAILED_TO_REBUILD_OPTIONS_FILE = "Failed to rebuild options file, closing\n";
     // Default strings
-    static const std::string DEFAULT_FONT = "../resources/fonts/EvilEmpire.otf";
+    static const std::string DEFAULT_FONT_FILE = "../resources/fonts/EvilEmpire.otf";
     static const std::string PLAY_BUTTON = "../resources/buttons/play_2.png";
     static const std::string OPTIONS_FILE_NAME = "options.cfg";
     static const std::string ERROR_FILE_NAME = "error.log";
@@ -35,8 +37,9 @@ namespace Constants {
     // Codes
     static const std::string DEBUG_CODE = "DEBUG";
     static int ERROR_OBJECT_ID = -999;
-
+    // Objects
     static auto DEFAULT_TEXTURE = sf::Texture(PLAY_BUTTON);
+    static auto DEFAULT_FONT = sf::Font(DEFAULT_FONT_FILE);
 }
 
 #endif //SFMLCODINGPROJECT_CONSTANTS_H
